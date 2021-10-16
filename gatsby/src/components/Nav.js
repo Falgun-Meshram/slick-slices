d1f879c680b943f007682063df31008bef732e2f
Falgun Meshram <falgun@pop-os.localdomain>
2021-10-16 19:27:55 -0400

    Added styled components

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -1,24 +1,74 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
+import Logo from './Logo';
+
+const NavStyles = styled.nav`
+  margin-bottom: 3rem;
+  ul {
+    margin: 0;
+    padding: 0;
+    text-align: center;
+    list-style: none;
+
+    display: grid;
+    grid-template-columns: 1fr 1fr auto 1fr 1fr;
+    grid-gap: 2rem;
+    align-items: center;
+  }
+  .logo {
+    transform: translateY(-25%);
+  }
+  li {
+    --rotate: 2deg;
+    margin-top: -6rem;
+
+    transform: rotate(var(--rotate));
+    order: 1;
+    &:nth-child(1) {
+      --rotate: 1deg;
+    }
+    &:nth-child(2) {
+      --rotate: -2.5deg;
+    }
+    &:nth-child(4) {
+      --rotate: 2.5deg;
+    }
+    &:hover {
+      --rotate: 6deg;
+    }
+  }
+  a {
+    font-size: 3rem;
+    text-decoration: none;
+    &:hover {
+      color: var(--red);
+    }
+  }
+`;
 
 export default function Nav() {
   return (
-    <ul>
-      <li>
-        <Link to="/">Hot Now</Link>
-      </li>
-      <li>
-        <Link to="/pizzas">Pizzas Menu</Link>
-      </li>
-      <li>
-        <Link to="">LOGO</Link>
-      </li>
-      <li>
-        <Link to="/slicemasters">Slice Masters</Link>
-      </li>
-      <li>
-        <Link to="/order"> Order Now</Link>
-      </li>
-    </ul>
+    <NavStyles>
+      <ul>
+        <li>
+          <Link to="/">Hot Now</Link>
+        </li>
+        <li>
+          <Link to="/pizzas">Pizzas Menu</Link>
+        </li>
+        <li>
+          <Link to="">
+            <Logo />
+          </Link>
+        </li>
+        <li>
+          <Link to="/slicemasters">Slice Masters</Link>
+        </li>
+        <li>
+          <Link to="/order"> Order Now</Link>
+        </li>
+      </ul>
+    </NavStyles>
   );
 }
