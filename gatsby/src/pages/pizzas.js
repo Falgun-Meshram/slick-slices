17be5681ac4ad6338156ba946b51ab838731312c
Falgun Meshram <falgun@pop-os.localdomain>
2021-10-17 12:37:59 -0400

    Added first grapql query

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,10 +1,37 @@
+import { graphql } from 'gatsby';
 import React from 'react';
 import Nav from '../components/Nav';
+import PizzaList from '../components/PizzaList';
 
-export default function PizzasPage() {
+export default function PizzasPage({ data }) {
   return (
     <div>
-      <p>Hey I am a pizzas page</p>
+      <PizzaList pizzas={data.pizzas.nodes} />
     </div>
   );
 }
+
+export const query = graphql`
+  query PizzaQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        topping {
+          topping
+          id
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
