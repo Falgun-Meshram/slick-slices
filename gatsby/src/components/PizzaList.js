ea0f42a44eeed635d59a091efc677be1b704f7ca
Falgun Meshram <falgun@pop-os.localdomain>
2021-10-17 12:38:33 -0400

    Component to render the list of pizzas

---
+++ gatsby/src/components/PizzaList.js
@@ -0,0 +1,25 @@
+import { Link } from 'gatsby';
+import React from 'react';
+
+function SinglePizza({ pizza }) {
+  return (
+    <div>
+      <Link to={`/pizza/${pizza.slug.current}`}>
+        <h2>
+          <span className="mark">{pizza.name}</span>
+        </h2>
+      </Link>
+      <p>{pizza.topping.map((top) => ` ${top.topping} |`)}</p>
+    </div>
+  );
+}
+
+export default function PizzaList({ pizzas }) {
+  return (
+    <div>
+      {pizzas.map((pizza) => (
+        <SinglePizza key={pizzas.id} pizza={pizza} />
+      ))}
+    </div>
+  );
+}
