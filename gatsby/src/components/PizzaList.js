af3433cb5e6a8ef3497efd76a453c548e68a3ec3
Falgun Meshram <falgun@pop-os.localdomain>
2021-10-30 16:14:41 -0400

    Added css grid

--- gatsby/src/components/PizzaList.js
+++ gatsby/src/components/PizzaList.js
@@ -1,25 +1,46 @@
-import { Link } from 'gatsby';
 import React from 'react';
+import { Link } from 'gatsby';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
+
+const PizzaGridStyles = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 4rem;
+  grid-auto-rows: auto auto 500px;
+`;
+
+const PizzaStyles = styled.div`
+  display: grid;
+  grid-template-rows: auto auto 1fr;
+  grid-row: span 3;
+  grid-gap: 1rem;
+  h2,
+  p {
+    margin: 0;
+  }
+`;
 
 function SinglePizza({ pizza }) {
   return (
-    <div>
+    <PizzaStyles>
       <Link to={`/pizza/${pizza.slug.current}`}>
         <h2>
           <span className="mark">{pizza.name}</span>
         </h2>
       </Link>
-      <p>{pizza.topping.map((top) => ` ${top.topping} |`)}</p>
-    </div>
+      <p>{pizza.toppings.map((top) => ` ${top.name} |`)}</p>
+      <Img fluid={pizza.image.asset.fluid} alt={pizza.name} />
+    </PizzaStyles>
   );
 }
 
 export default function PizzaList({ pizzas }) {
   return (
-    <div>
+    <PizzaGridStyles>
       {pizzas.map((pizza) => (
         <SinglePizza key={pizzas.id} pizza={pizza} />
       ))}
-    </div>
+    </PizzaGridStyles>
   );
 }
